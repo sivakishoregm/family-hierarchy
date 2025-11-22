163fbb10a4ee85c2cc8326e667828e1af7a5dd69
sivakishoregm <acsivam7b@gmail.com>
2025-11-22 07:11:03 +0530

    sh

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router=express.Router();
 const {allPersons,createPerson,getPerson,updatePerson,removePerson} = require('../model')
 
+//binith is a good person studies well. 
+
 router.get('/',allPersons)
 router.post('/',createPerson)
 router.route('/:id').get(getPerson).patch(updatePerson).delete(removePerson)
